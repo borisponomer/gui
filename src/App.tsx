2d60d8ff173e2b31585cebd83164f3fc13bc5ac3
Boris Ponomarenko <borisponomarenko.dmi@gmail.com>
2023-03-26 12:40:15 +0300

    Add feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Greet } from './Greet/Greet';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
             <h1>Hello world</h1>
+            <Greet />
         </div>
     );
 }
