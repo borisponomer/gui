7b0c5dd2c1be3a295af6ff7e74cdab384a2ad1fd
Boris <65194683+ponomarenkoboris@users.noreply.github.com>
2023-03-26 13:02:03 +0300

    Deleting readme file + add input in greetimg component (#1)
    * Add feature
    
    * Remove readme file

--- src/Greet/Greet.tsx
+++ src/Greet/Greet.tsx
@@ -1,5 +1,17 @@
+import { useState, ChangeEvent } from 'react'
+
 export const Greet = ({ name }: { name?: string }) => {
+    const [value, setValue] = useState('')
+
+    const changeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value)
+    }
+
     return (
-        <div className="jo">{name ? `Greet, ${name}!` : 'Greet'}</div>
+        <div className="jo">
+            <p>{name ? `Greet, ${name}!` : 'Greet'}</p>
+            <h1 style={{ color: value.length % 2 === 0 ? 'red' : 'blue' }}>{value}</h1>
+            <input type="text" onChange={changeInputHandler} />
+        </div>
     )
 }
